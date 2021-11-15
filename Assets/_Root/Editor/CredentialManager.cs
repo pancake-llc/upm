3753bb1df1667d2d3d95ab8bf9f7cc0365c1f50c
yenmoc <phongsoyenmoc.diep@gmail.com>
2021-11-16 00:27:01 +0700

    *wip add class credential

---
+++ Assets/_Root/Editor/CredentialManager.cs
@@ -0,0 +1,15 @@
+namespace com.snorlax.upm
+{
+    public class Credential
+    {
+        public string npmAuth;
+        public string token;
+        public string email;
+        public bool alwayAuth;
+    }
+
+
+    public class CredentialManager
+    {
+    }
+}
